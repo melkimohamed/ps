d77466239c1115e8d6560ca7296531a84a914f4d
melkimohamed <mohamedmelki@hotmail.fr>
2018-02-18 23:17:37 +0100

    push nodejs

---
+++ nodejs/tuto crud/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html')
+})
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
